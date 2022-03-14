b1ff87dc0f6f133d084b3dc7cd19808329e8f749
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-03-14 12:41:47 +0100

    make installable package by adding the necessary files

---
+++ src/atmcirclib/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package ``atmcirclib``."""
+# Standard library
+import importlib.metadata as md
+
+__author__ = """Stefan Ruedisuehli"""
+__email__ = "stefan.ruedisuehli@env.ethz.ch"
+__version__ = md.version(__package__ or __name__)
