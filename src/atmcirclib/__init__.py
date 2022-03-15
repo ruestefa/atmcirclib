e4fa6152626236f8d1f772dec5a48f1644dbfb9a
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-03-15 14:06:40 +0100

    improve meta data import

--- src/atmcirclib/__init__.py
+++ src/atmcirclib/__init__.py
@@ -1,7 +1,9 @@
 """Top-level package ``atmcirclib``."""
 # Standard library
-import importlib.metadata as md
+import importlib.metadata
+
+meta = importlib.metadata.metadata(__package__)
 
 __author__ = """Stefan Ruedisuehli"""
 __email__ = "stefan.ruedisuehli@env.ethz.ch"
-__version__ = md.version(__package__ or __name__)
+__version__ = importlib.metadata.version(__package__)
