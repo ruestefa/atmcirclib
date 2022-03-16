2fbf017340a14a1ec7cb27f653098b850a414ee7
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-03-16 13:17:05 +0100

    add package-level imports

--- src/atmcirclib/__init__.py
+++ src/atmcirclib/__init__.py
@@ -2,8 +2,13 @@
 # Standard library
 import importlib.metadata
 
-meta = importlib.metadata.metadata(__package__)
+# Local
+from . import traj
+
+metadata = importlib.metadata.metadata(__package__)
 
 __author__ = """Stefan Ruedisuehli"""
 __email__ = "stefan.ruedisuehli@env.ethz.ch"
 __version__ = importlib.metadata.version(__package__)
+
+del importlib
