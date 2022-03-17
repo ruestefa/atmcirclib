323bee349d2b0efb173e213649632610d4785c57
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-03-17 09:18:30 +0100

    simplify PathLike_T and add error code to noqa directive

--- src/atmcirclib/typing.py
+++ src/atmcirclib/typing.py
@@ -6,7 +6,7 @@ from typing import Union
 
 if typing.TYPE_CHECKING:
     # Standard library
-    from os import PathLike  # noqa
+    from os import PathLike  # noqa: F401  # imported but unused
 
 PathLike_T = Union[str, os.PathLike]
 PathLikeAny_T = Union[str, os.PathLike, bytes]
