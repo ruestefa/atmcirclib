0dba35af2748c8682c86c3f95da43a44eef804ab
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-03-24 14:43:54 +0100

    make scaling of z from m to km optional and explicit

--- src/atmcirclib/cosmo.py
+++ src/atmcirclib/cosmo.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 # Standard library
+from typing import Any
 from typing import cast
 
 # Third-party
@@ -16,9 +17,10 @@ from atmcirclib.typing import PathLike_T
 class COSMOGridDataset:
     """File with grid information of COSMO simulation."""
 
-    def __init__(self, ds: xr.Dataset) -> None:
+    def __init__(self, ds: xr.Dataset, z_unit: str = "m") -> None:
         """Create new instance."""
         self.ds: xr.Dataset = ds
+        self.z_unit: str = z_unit
 
     def get_bbox_xy(self) -> BoundingBox:
         """Get (lon, lat) bounding box."""
@@ -29,12 +31,11 @@ class COSMOGridDataset:
         bbox_xy = self.get_bbox_xy()
         zmin = 0.0
         zmax = float(self.ds.height_toa.data)
-        zmax /= 1000.0  # m => km  # TODO make this more explicit
         return BoundingBox(
             llx=bbox_xy.llx,
             urx=bbox_xy.urx,
-            lly=zmin,
-            ury=zmax,
+            lly=self._scale_z(zmin),
+            ury=self._scale_z(zmax),
         )
 
     def get_bbox_yz(self) -> BoundingBox:
@@ -56,9 +57,24 @@ class COSMOGridDataset:
             pole_longitude=self.ds.rotated_pole.grid_north_pole_longitude,
         )
 
+    def _scale_z(self, val: float) -> float:
+        """Scale height value to desired unit ``self.z_unit``."""
+        unit = self.ds.height_toa.attrs["units"]
+        if self.z_unit == unit:
+            fact = 1.0
+        elif self.z_unit == "km" and unit == "m":
+            fact = 1 / 1000.0
+        elif self.z_unit == "m" and unit == "km":
+            fact = 1000.0
+        else:
+            raise Exception(
+                f"unknown target z unit '{self.z_unit}' or input z unit '{unit}'"
+            )
+        return fact * val
+
     @classmethod
-    def from_file(cls, path: PathLike_T) -> COSMOGridDataset:
+    def from_file(cls, path: PathLike_T, **kwargs: Any) -> COSMOGridDataset:
         """Create an instance from a NetCDF file."""
         # mypy thinks return type is Any (mypy v0.941, numpy v1.22.3)
         ds: xr.Dataset = cast(xr.Dataset, xr.open_dataset(path))
-        return cls(ds)
+        return cls(ds, **kwargs)
