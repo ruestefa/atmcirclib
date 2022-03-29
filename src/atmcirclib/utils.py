063125e17e4bb679bbb51e8eff8eabdcddff4952
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-03-29 23:28:26 +0200

    add partial_format from cosmo_nawdex

--- src/atmcirclib/utils.py
+++ src/atmcirclib/utils.py
@@ -1,2 +1,17 @@
 """Various utilities."""
 from __future__ import annotations
+
+# Standard library
+import re
+from typing import Any
+
+
+def partial_format(s: str, *, max_try: int = 99, **keys: Any) -> str:
+    """Format ``s``, leaving format specifiers not in ``keys`` as they are."""
+    for i in range(max_try):
+        try:
+            return s.format(**keys)
+        except KeyError as e:
+            key = str(e).lstrip("\"'").rstrip("\"'")
+            s = re.sub(r"(?<!{)(?P<key>{" + key + "(:[^}]*)?})(?!})", r"{\g<key>}", s)
+    raise ValueError(f"partial formatting unsuccessful after {max_try} tries: '{s}'")
