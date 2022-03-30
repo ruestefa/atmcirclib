c6062d1137404fbea5cca88cbfc28cb12d756bbb
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-03-31 00:59:45 +0200

    add target as optional argument to get_data script

--- get_data.sh
+++ get_data.sh
@@ -3,6 +3,8 @@
 # Copy test data useful as reference to mock data files in tests
 # (The data files are not required to run the unit tests)
 
-loc="daint:/project/s1063/ruestefa/data/shared/atmcirclib/"
+target="${1:-.}"
 
-\rsync -au --progress "${loc}/data" .
+source="daint:/project/s1063/ruestefa/data/shared/atmcirclib/"
+
+\rsync -au --progress "${source}/data" "${target}"
