b95c5c0a95ef85a37d0d527e2cdd15cdbf24137d
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-03-30 09:33:00 +0200

    fix dummy variable name

--- src/atmcirclib/utils.py
+++ src/atmcirclib/utils.py
@@ -8,7 +8,7 @@ from typing import Any
 
 def partial_format(s: str, *, max_try: int = 99, **keys: Any) -> str:
     """Format ``s``, leaving format specifiers not in ``keys`` as they are."""
-    for i in range(max_try):
+    for _ in range(max_try):
         try:
             return s.format(**keys)
         except KeyError as e:
