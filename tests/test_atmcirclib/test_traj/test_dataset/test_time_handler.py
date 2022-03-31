b6ea72a3bf0731a1713b504b9039a2e356198524
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-03-31 19:54:12 +0200

    restructure test helper function

--- tests/test_atmcirclib/test_traj/test_dataset/test_time_handler.py
+++ tests/test_atmcirclib/test_traj/test_dataset/test_time_handler.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 # Standard library
 import datetime as dt
-from typing import Any
 from typing import Optional
 
 # Third-party
@@ -22,36 +21,40 @@ from .shared import create_traj_dataset_ds
 
 
 def create_time_handler(
+    year: int,
+    month: int = 1,
+    day: int = 1,
+    hour: int = 0,
+    min: int = 0,
+    sec: int = 0,
+    *,
     steps: npt.ArrayLike,
     dtype: npt.DTypeLike = "timedelta64[m]",
-    *,
     step_sec: float,
     dur_sec: float,
     model: Optional[str] = None,
-    **attrs: Any,
 ) -> TrajTimeHandler:
     """Create a traj time handler from given time steps and attributes."""
     if isinstance(dtype, str) and dtype.startswith("[") and dtype.endswith("]"):
         # Shorthand for timedelta: Unit only, e.g., '[h]', '[m]', '[s]'
         dtype = f"timedelta64{dtype}"
-    default_attrs = {
-        "ref_year": 2016,
-        "ref_month": 9,
-        "ref_day": 20,
-        "ref_hour": 0,
-        "ref_min": 0,
-        "ref_sec": 0,
-        "duration_in_sec": dur_sec,
-        "pollon": 178.0,
-        "pollat": 30.0,
-        "output_timestep_in_sec": step_sec,
-    }
     ds = create_traj_dataset_ds(
         dims=("time", "id"),
         coords_d={"time": steps},
         dtype_d={"time": dtype},
         # data_d={"z": np.ones([len(steps), 10])},
-        attrs={**default_attrs, **attrs},
+        attrs={
+            "ref_year": year,
+            "ref_month": month,
+            "ref_day": day,
+            "ref_hour": hour,
+            "ref_min": min,
+            "ref_sec": sec,
+            "duration_in_sec": dur_sec,
+            "pollon": 178.0,
+            "pollat": 30.0,
+            "output_timestep_in_sec": step_sec,
+        },
     )
     return TrajTimeHandler(TrajDataset(ds, model=model))
 
@@ -61,16 +64,11 @@ class Test_GetStart:
 
     def test_forward(self) -> None:
         """Get start time of forward simulation."""
-        ref_kw: dict[str, Any] = {
-            "ref_year": 1990,
-            "ref_month": 2,
-            "ref_day": 24,
-            "ref_hour": 21,
-        }
+        ref = (1990, 2, 24, 21)
         th = create_time_handler(
-            np.arange(10), "[m]", step_sec=60, dur_sec=3600, **ref_kw
+            *ref, steps=np.arange(10), dtype="[m]", step_sec=60, dur_sec=3600
         )
-        ref_dt = dt.datetime(*ref_kw.values())
+        ref_dt = dt.datetime(*ref)
         assert th.get_start() == ref_dt
 
     def test_forward_cosmo(self) -> None:
@@ -87,16 +85,16 @@ class Test_GetStart:
         output time step, which differ for ``ninc_out_traj > 0``.)
 
         """
-        ref_kw: dict[str, Any] = {
-            "ref_year": 1990,
-            "ref_month": 2,
-            "ref_day": 24,
-            "ref_hour": 21,
-        }
+        ref = (1990, 2, 24, 21)
         th = create_time_handler(
-            np.arange(10), "[m]", step_sec=60, dur_sec=3600, model="cosmo", **ref_kw
+            *ref,
+            steps=np.arange(10),
+            dtype="[m]",
+            step_sec=60,
+            dur_sec=3600,
+            model="cosmo",
         )
-        ref_dt = dt.datetime(*ref_kw.values()) + dt.timedelta(seconds=60)
+        ref_dt = dt.datetime(*ref) + dt.timedelta(seconds=60)
         assert th.get_start() == ref_dt
 
     def test_forward_lagranto(self) -> None:
@@ -105,35 +103,36 @@ class Test_GetStart:
         The result is the same as w/o specifying a model.
 
         """
-        ref_kw: dict[str, Any] = {
-            "ref_year": 1990,
-            "ref_month": 2,
-            "ref_day": 24,
-            "ref_hour": 21,
-        }
+        ref = (1990, 2, 24, 21)
         th = create_time_handler(
-            np.arange(10), "[m]", step_sec=60, dur_sec=3600, model="lagranto", **ref_kw
+            *ref,
+            steps=np.arange(10),
+            dtype="[m]",
+            step_sec=60,
+            dur_sec=3600,
+            model="lagranto",
         )
-        ref_dt = dt.datetime(*ref_kw.values())
+        ref_dt = dt.datetime(*ref)
         assert th.get_start() == ref_dt
 
     def test_backward(self) -> None:
         """Get start time of backward simulation."""
-        ref_kw: dict[str, Any] = {
-            "ref_year": 1990,
-            "ref_month": 2,
-            "ref_day": 24,
-            "ref_hour": 21,
-        }
+        ref = (1990, 2, 24, 21)
         th = create_time_handler(
-            -np.arange(10), "[m]", step_sec=-60, dur_sec=-3600, **ref_kw
+            *ref, steps=-np.arange(10), dtype="[m]", step_sec=-60, dur_sec=-3600
         )
-        ref_dt = dt.datetime(*ref_kw.values())
+        ref_dt = dt.datetime(*ref)
         assert th.get_start() == ref_dt
 
     def test_backward_cosmo_fail(self) -> None:
         """Backward simulation with COSMO is not possible."""
         with pytest.raises(ValueError):
             create_time_handler(
-                -np.arange(10), "[m]", step_sec=-60, dur_sec=-3600, model="cosmo"
+                1990,
+                2,
+                steps=-np.arange(10),
+                dtype="[m]",
+                step_sec=-60,
+                dur_sec=-3600,
+                model="cosmo",
             )
