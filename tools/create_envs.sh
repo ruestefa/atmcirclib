be049e935a2542d8eb41c62e0cbe4d3e0ba043bb
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-04-14 17:25:14 +0200

    if available, use mamba instead of conda; echo mamba/conda commands

--- tools/create_envs.sh
+++ tools/create_envs.sh
@@ -9,6 +9,27 @@ dev_reqs_file="dev-requirements.in"
 run_env_file="environment.yml"
 dev_env_file="dev-environment.yml"
 
+detect_conda()
+{
+    mamba --version 1>/dev/null 2>&1
+    if [[ ${?} -eq 0 ]]; then
+        echo "MAMBA_NO_BANNER=1 mamba"
+        return 0
+    fi
+    conda --version 1>/dev/null 2>&1
+    if [[ ${?} -eq 0 ]]; then
+        echo conda
+        return 0
+    fi
+    echo "error: neither mamba nor conda detectec" >&2
+    return 1
+}
+
+CONDA=$(detect_conda) || exit
+cmd=(${CONDA} --version)
+echo "\$ ${cmd[@]^Q}"
+eval "${cmd[@]}" || exit
+
 create_env()
 {
     local env_name="${1}"
@@ -19,22 +40,28 @@ create_env()
     local reqs_file
 
     if [[ "${CONDA_PREFIX}" != "" ]]; then
-        echo "deactivate conda env '${CONDA_PREFIX}'"
-        conda deactivate || return 1
+        echo "please deactivate conda env and retry (detected '${CONDA_PREFIX}')" >&2
+        return 1
     fi
-    if $(conda info --env | \grep -q "^\<${env_name}\>"); then
+    if $(eval ${CONDA} info --env | \grep -q "^\<${env_name}\>"); then
         echo "remove conda env '${env_name}'"
-        conda env remove -n "${env_name}" || return 1
+        cmd=(${CONDA} env remove -n "${env_name}")
+        echo "\$ ${cmd[@]^Q}"
+        eval "${cmd[@]}" || return 1
     fi
 
     echo "create conda env '${env_name}' from ${reqs_files[@]}"
     for reqs_file in "${reqs_files[@]}"; do
         reqs_file_flags+=(--file="${reqs_file}")
     done
-    conda create -n "${env_name}" python==${PYTHON_VERSION} "${reqs_file_flags[@]}" --yes || return 1
+    cmd=(${CONDA} create -n "${env_name}" python==${PYTHON_VERSION} "${reqs_file_flags[@]}" --yes )
+    echo "\$ ${cmd[@]^Q}"
+    eval "${cmd[@]}" || return 1
 
     echo "export conda env '${env_name}' to ${env_file}"
-    conda env export -n "${env_name}" --no-builds > "${env_file}" || return 1
+    cmd=(${CONDA} env export -n "${env_name}" --no-builds)
+    echo "\$ ${cmd[@]^Q}"
+    eval "${cmd[@]}" > "${env_file}" || return 1
 
     return 0
 }
