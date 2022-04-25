5b914a379acab9aad9eeac1f1368bc17371140c4
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-04-25 20:24:08 +0200

    bump version and create recipe

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from skbuild import setup
 PackageDataT = Dict[str, List[str]]
 
 PROJECT_NAME: str = "atmcirclib"
-PROJECT_VERSION: str = "0.2.2"
+PROJECT_VERSION: str = "0.2.3"
 PYTHON_REQUIRES: str = ">=3.9"
 
 
