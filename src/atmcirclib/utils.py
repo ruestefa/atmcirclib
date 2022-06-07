54ea16d5e919748433f4777d9a3917cb3e7b4f02
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-06-07 15:39:44 +0200

    add function exception_as_error to module utils (from cosmo_nawdex.simulations.bin)

--- src/atmcirclib/utils.py
+++ src/atmcirclib/utils.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 
 # Standard library
 import re
+import sys
+from functools import wraps
 from typing import Any
+from typing import Callable
 
 
 def partial_format(s: str, *, max_try: int = 99, **keys: Any) -> str:
@@ -15,3 +18,28 @@ def partial_format(s: str, *, max_try: int = 99, **keys: Any) -> str:
             key = str(e).lstrip("\"'").rstrip("\"'")
             s = re.sub(r"(?<!{)(?P<key>{" + key + "(:[^}]*)?})(?!})", r"{\g<key>}", s)
     raise ValueError(f"partial formatting unsuccessful after {max_try} tries: '{s}'")
+
+
+def exception_as_error(
+    func: Callable[..., Any],
+    print_: Callable[..., None] = print,
+    exit_: Callable[[int], None] = sys.exit,
+) -> Callable[..., Any]:
+    """Decorate/wrap a function to replace an exception by an error message."""
+
+    @wraps(func)
+    def wrapped(*args: Any, **kwargs: Any) -> Any:
+        """Catch any exception raised by ``func``, print the message and exit."""
+        try:
+            # mypy 0.941 thinks return value has type Any
+            return func(*args, **kwargs)
+        except Exception as ex:
+            ex_name = type(ex).__name__
+            msg = "error"
+            if ex_name != "Exception":
+                msg += f" ({ex_name})"
+            msg += f": {ex}"
+            print_(msg, file=sys.stderr)
+            exit_(1)
+
+    return wrapped
