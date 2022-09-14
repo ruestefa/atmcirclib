a472ecea586ddcb3e85ca94527f7e69425211217
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-09-14 15:53:56 +0200

    catch import error of extension module

--- src/atmcirclib/deriv/__init__.py
+++ src/atmcirclib/deriv/__init__.py
@@ -1,4 +1,12 @@
 """Subpackage ``atmcirclib.deriv``."""
+from __future__ import annotations
 
-# Local
-from . import ext
+# Standard library
+from warnings import warn
+
+try:
+    # Local
+    from . import ext
+except ImportError as e:
+    warn(f"cannot import atmcirclib.deriv.ext: {e}")
+    ext = None  # type: ignore
